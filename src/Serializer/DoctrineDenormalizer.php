039b1a1ad9e3b5364529f23f5bcf567f0f769fb4
Ketan Padia <ketan@loyaltycorp.com.au>
2019-06-11 15:17:44 +1000

    Add custom find mapping in denormalizer

--- src/Serializer/DoctrineDenormalizer.php
+++ src/Serializer/DoctrineDenormalizer.php
@@ -12,6 +12,8 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
  */
 class DoctrineDenormalizer implements DenormalizerInterface
 {
+    private $findKeyMap;
+
     /**
      * @var \Doctrine\Common\Persistence\ManagerRegistry
      */
@@ -22,9 +24,10 @@ class DoctrineDenormalizer implements DenormalizerInterface
      *
      * @param \Doctrine\Common\Persistence\ManagerRegistry $managerRegistry
      */
-    public function __construct(ManagerRegistry $managerRegistry)
+    public function __construct(ManagerRegistry $managerRegistry, ?array $findKeyMap = null)
     {
         $this->managerRegistry = $managerRegistry;
+        $this->findKeyMap = $findKeyMap;
     }
 
     /**
@@ -38,6 +41,14 @@ class DoctrineDenormalizer implements DenormalizerInterface
             // of deserialize
 
             return $data;
+        }
+
+        // find key from provided mapping
+        $findKey = $this->getFindByKey($class);
+
+        if ($findKey !== null) {
+            return $this->managerRegistry->getRepository($class)
+                ->findOneBy([$findKey => $data[$findKey]]);
         }
 
         if (($data['id'] ?? null) === null) {
@@ -59,5 +70,25 @@ class DoctrineDenormalizer implements DenormalizerInterface
         }
 
         return $manager->getMetadataFactory()->isTransient($type) === false;
+    }
+
+    /**
+     * Get find by key from provided mappings.
+     *
+     * @param $class
+     *
+     * @return bool|null
+     */
+    public function getFindByKey($class): ?string
+    {
+        if ($this->findKeyMap === null) {
+            return null;
+        }
+
+        if (\array_key_exists($class, $this->findKeyMap) !== true) {
+            return null;
+        }
+
+        return $this->findKeyMap[$class];
     }
 }
