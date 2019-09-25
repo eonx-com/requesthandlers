feba7d9805f73aaec22f9f486148728960da37f5
Tim Nagel <tim@nagel.com.au>
2019-09-25 13:14:07 +1000

    Change wording

--- src/Annotations/InjectedFromContext.php
+++ src/Annotations/InjectedFromContext.php
@@ -5,7 +5,7 @@ namespace LoyaltyCorp\RequestHandlers\Annotations;
 
 /**
  * This annotation is used on properties to indicate that they should only be injected from
- * the context of the request and not be processed from the request body.
+ * the context of the request and not be populated with data from the request body.
  *
  * It allows for things like the object from the request URL to be resolved and injected
  * into the request object for additional validation purposes.
